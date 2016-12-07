b2ba753e4d6a19e70e5c32742d536cc7a0cff6a2
Don <donly@magicalboy.com>
2016-12-07 11:50:17 +0800

    simple webserver app

--- Sources/main.swift
+++ Sources/main.swift
@@ -1,1 +1,28 @@
-print("Hello, Weechun!")
+import PerfectLib
+import PerfectHTTP
+import PerfectHTTPServer
+
+// Create HTTP server.
+let server = HTTPServer()
+
+// Register your own routes and handlers
+var routes = Routes()
+routes.add(method: .get, uri: "/", handler: {
+    request, response in
+    response.setHeader(.contentType, value: "text/html")
+    response.appendBody(string: "<!DOCTYPE html><html><title>Wee Chun</title><body>Hello, Weechun!</body></html>")
+    response.completed()
+})
+
+// Add the routes to the server
+server.addRoutes(routes)
+
+// Set a listen port of 8080
+server.serverPort = 8080
+
+do {
+    // Launch the HTTP server.
+    try server.start()
+} catch PerfectError.networkError(let err, let msg) {
+    print("Network error thrown: \(err) \(msg)")
+}
